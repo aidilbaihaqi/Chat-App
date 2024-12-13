f3a79d3b6a57bfdbd9edf75aa118175c2768d5eb
aidilbaihaqi <aidillmusirjun@gmail.com>
2024-12-13 16:34:45 +0700

    make controller

--- app/Http/Controllers/ChatController.php
+++ app/Http/Controllers/ChatController.php
@@ -2,9 +2,28 @@
 
 namespace App\Http\Controllers;
 
+use App\Events\MessageSent;
+use App\Models\Message;
 use Illuminate\Http\Request;
 
 class ChatController extends Controller
 {
-    //
+    public function index()
+    {
+        $messages = Message::where('from_user_id', auth()->id)
+                    ->orWhere('to_user_id', auth()->id)->get();
+        return view('chat.index', compact('messages'));
+    }
+
+    public function sendMessage(Request $request)
+    {
+        $message = Message::create([
+            'from_user_id' => auth()->id,
+            'to_user_id' => $request->to_user_id,
+            'message' => $request->message
+        ]);
+
+        broadcast(new MessageSent($message))->toOthers();
+        return response()->json(['message'=> $message]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Http\Controllers\ChatController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::middleware('auth')->group(function () {
+    Route::get('/chat', [ChatController::class, 'index'])->name('chat.index');
+    Route::post('/chat/send', [ChatController::class, 'sendMessage'])->name('chat.send');
 });
